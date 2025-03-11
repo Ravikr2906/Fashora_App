313722e0bc5128b647d2e600f5b12652b9f7917b
Ravi kumar <ravikr2906@gmail.com>
2025-03-11 16:57:20 +0530

    Fix responsiveness ProductGrid.jsx

--- Client/src/components/products/ProductGrid.jsx
+++ Client/src/components/products/ProductGrid.jsx
@@ -9,27 +9,28 @@ const ProductGrid = ({ products, loading, error }) => {
   if(error)  return <p>Error: {error}</p> 
   
   return (
-    <div className="container ">
-      <div className="row g-4">
-        {products.map((product, index) => (
-          <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3">
-            <Link key={index} to={`/product/${product._id}`} className="text-decoration-none">
-              <div className=" rounded">
-                <div className="w-100 mb-2 ">
-                  <img 
-                    src={product.images[0]?.url} 
-                    alt={product.images[0]?.alt || product.name} 
-                    className="w-100 object-fit-cover rounded me-2" height={400} 
-                  />
-                </div>
-                <h6 className='text-dark'>{product.name}</h6>
-                <p className="text-muted">${product.price}</p>
-              </div>
-            </Link>
+    <div className="container">
+  <div className="row g-4">
+    {products.map((product, index) => (
+      <div key={index} className="col-12 col-sm-6 col-md-4 col-lg-3">
+        <Link to={`/product/${product._id}`} className="text-decoration-none">
+          <div className="rounded">
+            <div className="w-100 mb-2">
+              <img 
+                src={product.images[0]?.url} 
+                alt={product.images[0]?.alt || product.name} 
+                className="w-100 object-fit-cover rounded responsive-img"
+              />
+            </div>
+            <h6 className='text-dark'>{product.name}</h6>
+            <p className="text-muted">${product.price}</p>
           </div>
-        ))}
+        </Link>
       </div>
-    </div>
+    ))}
+  </div>
+</div>
+
   );
 }
 
